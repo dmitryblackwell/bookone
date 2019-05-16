2ca59a5666e370770d89fea35d576e1c778bb099
Dmitro Veretelnyk <dmitrov@SYTOSS.INTRA>
2019-05-16 21:54:38 +0300

    Add exception hanler properite

--- src/main/java/com/blackwell/aop/ExceptionHandlerController.java
+++ src/main/java/com/blackwell/aop/ExceptionHandlerController.java
@@ -1,16 +1,28 @@
 package com.blackwell.aop;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
 
 @ControllerAdvice
 public class ExceptionHandlerController {
+
+    @Getter @Setter
+    @Value("${exception.handler.on}")
+    private boolean isOn;
 	
-//	@ExceptionHandler	
-//	public String handleException(Exception ex, Model model) {
-//		model.addAttribute("message", ex.getMessage());
-//		return "errorpage";
-//	}
+	@ExceptionHandler
+	public ModelAndView handleException(Exception ex) throws Exception {
+	    if (!isOn) {
+            throw ex;
+        }
+        ModelAndView modelAndView = new ModelAndView("errorpage");
+        modelAndView.addObject("message", ex.getMessage());
+        return modelAndView;
+	}
 	
 }
